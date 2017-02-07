481f08501f21558be06ff5345d9521bc9245837d
Coda Hale <coda.hale@gmail.com>
2017-02-06 17:27:06 -0800

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codahale/xsalsa20poly1305 "0.1.0"
+(defproject com.codahale/xsalsa20poly1305 "0.1.1-SNAPSHOT"
   :description "A Clojure wrapper for XSalsa20Poly1305 encryption."
   :url "https://github.com/codahale/xsalsa20poly1305"
   :license {:name "Eclipse Public License"
