caa53eeff31725d6299ad6b784e110a1ae904475
Coda Hale <coda.hale@gmail.com>
2017-02-16 06:58:07 -0800

    Switch simplebox to misuse-resistant nonces.

--- src/xsalsa20poly1305/simplebox.clj
+++ src/xsalsa20poly1305/simplebox.clj
@@ -1,14 +1,15 @@
 (ns xsalsa20poly1305.simplebox
   "Convenience functions for encryption without requiring nonce management.
 
-  Compatible with RbNaCl's Simplebox construction."
+  Compatible with RbNaCl's Simplebox construction, but generates
+  misuse-resistant nonces."
   (:require [xsalsa20poly1305.core :refer [nonce-size] :as xsalsa20poly1305]))
 
 (defn seal
   "Encrypts the given plaintext with the given key, returning the ciphertext.
   The key must be 32 bytes long."
   ^bytes [^bytes k ^bytes p]
-  (let [n (xsalsa20poly1305/generate-nonce)
+  (let [n (xsalsa20poly1305/generate-nmr-nonce k p)
         c (xsalsa20poly1305/seal k n p)
         o (byte-array (+ (count c) nonce-size))]
     (System/arraycopy n 0 o 0 nonce-size)
