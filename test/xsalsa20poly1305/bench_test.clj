4aef84c07f3d4a6e73354f09b34d8f1825adf616
Coda Hale <coda.hale@gmail.com>
2017-02-16 06:57:00 -0800

    Add benchmarks for simplebox and Caesium.

--- test/xsalsa20poly1305/bench_test.clj
+++ test/xsalsa20poly1305/bench_test.clj
@@ -1,17 +1,31 @@
 (ns xsalsa20poly1305.bench-test
-  (:require [clojure.test :refer :all]
+  (:require [caesium.crypto.secretbox :as secretbox]
+            [clojure.test :refer :all]
             [criterium.core :as c]
-            [xsalsa20poly1305.core :refer :all]))
+            [xsalsa20poly1305.core :as xsalsa20poly1305]
+            [xsalsa20poly1305.simplebox :as simplebox]))
 
 (def n (byte-array 24))
 (def k (byte-array 32))
 
 (defn- sep
-  [n]
-  (printf "\n\n######  %d bytes  ######\n" n))
+  [s n]
+  (printf "\n\n######  %s/%d bytes  ######\n" s n))
 
-(deftest ^:bench bench-all
-  (doseq [i [100 1024 (* 10 1024) (* 100 1024)]]
-    (sep i)
+(deftest ^:bench bench-core
+  (doseq [i [100 1024 (* 10 1024)]]
+    (sep "core" i)
     (let [p (byte-array i)]
-      (c/bench (seal k n p)))))
+      (c/bench (xsalsa20poly1305/seal k n p)))))
+
+(deftest ^:bench bench-caesium
+  (doseq [i [100 1024 (* 10 1024)]]
+    (sep "caesium" i)
+    (let [p (byte-array i)]
+      (c/bench (secretbox/encrypt k n p)))))
+
+(deftest ^:bench bench-simplebox
+  (doseq [i [100 1024 (* 10 1024)]]
+    (sep "simplebox" i)
+    (let [p (byte-array i)]
+      (c/bench (simplebox/seal k p)))))
